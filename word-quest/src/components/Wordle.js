1b0ff0bf1ef5539974ccb57cb6daf6732312fd8e
Gaurav Sawant <gssawant_b17@it.vjti.ac.in>
2024-01-06 15:30:57 +0530

    Added modal functionality

--- word-quest/src/components/Wordle.js
+++ word-quest/src/components/Wordle.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 import { MAX_GUESSES } from '../constants'
 
 export default function Wordle({ solution }) {
   
   const { currentGuess, handleKeyUp, guesses, isCorrect, usedKeys, turn }  = useWordle(solution)
+  const [showModal, setShowModal] = useState(false)
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp)
     if (isCorrect) {
-      console.log(`You found the word correctly in ${turn} attempts!`)
+      setTimeout(setShowModal(true), 2000)
+      console.log(`You found the word correctly within ${turn} attempts!`)
       window.removeEventListener('keyup', handleKeyUp)
     }
     if (turn >= MAX_GUESSES) {
+      setTimeout(setShowModal(true), 2000)
       console.log(`You are out of attempts.. The word was ${solution}!`)
       window.removeEventListener('keyup', handleKeyUp)
     }
@@ -27,6 +31,7 @@ export default function Wordle({ solution }) {
       <div>Solution = { solution }</div> {/* for dev reference only */}
       <Grid currentGuess = { currentGuess } guesses = { guesses } turn = { turn } />
       <Keypad usedKeys = { usedKeys } />
+      {showModal && <Modal isCorrect = { isCorrect } turn = { turn } solution = { solution } />}
     </div>
   )
 }
